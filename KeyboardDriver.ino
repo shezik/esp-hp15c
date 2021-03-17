bcdf22c4eafccbd69d407c927a470ee0afff16be
Kerry Shen <shenzhikai@outlook.com>
2021-03-17 23:58:00 +0800

    Minor improvements
    Keyboard driver prototype implemented.

--- KeyboardDriver.ino
+++ KeyboardDriver.ino
@@ -12,6 +12,7 @@ const int INTCAP0    = 0x08;  // INTERRUPT CAPTURE REGISTER 0
 const int INTCAP1    = 0x09;  // INTERRUPT CAPTURE REGISTER 1
 const int IOCON0     = 0x0A;  // I/O EXPANDER CONTROL REGISTER 0
 const int IOCON1     = 0x0B;  // I/O EXPANDER CONTROL REGISTER 1
+
 const int MCPAddress = 0x20;  // Check out MCP23016 datasheet to find out how to determine this value
 const int MCPSDA     = D2;
 const int MCPSCL     = D1;
@@ -28,7 +29,7 @@ uint8_t readBlockData(uint8_t gp)
   int ret = Wire.endTransmission();
   //Serial.print("Wire.endTransmission() from readBlockData(uint8_t gp) finished with code ");
   //Serial.println(ret);
-  delay(10);
+  delay(10); // Essential delays, comm does not work without them. :(
   Wire.requestFrom(MCPAddress, 1); //read one byte from chip
   delay(10);
   return Wire.read();
@@ -46,45 +47,53 @@ void writeBlockData(uint8_t reg, uint8_t data)
 }
 
 int handleMCPKey() {
-  // 8-pin 4x4 keyboard scanner, one keystroke at a time
-  // running with MCP23016 GPIO expander
-  // This scanning method consumes less CPU resource when idle
 
-  //Serial.println();
-  //Serial.println("handleMCPKey() was called!");
-  
-  uint8_t coordCol, coordRow;
+    // 14-pin 10x4 keyboard scanner, one keystroke at a time
 
-  // Stage 1: Pull first 4 lanes high simultaneously to detect column
-  writeBlockData(GP0, B00001111);
-  coordCol = (readBlockData(GP0) & B11110000); // Only second 4 pins' status is kept. (one clue to possibly simplify this procedure: they'll always be greater or smaller in comparison to something, no idea if this could be useful or not)
-  writeBlockData(GP0, B00000000); // Cleanup
-  //Serial.print("Value of coordCol: ");
-  //Serial.println(coordCol);
+    uint8_t coordRow, coordCol = -1;
 
-  if (coordCol != 16 && coordCol != 32 && coordCol != 64 && coordCol != 128) {
-    //Serial.println("Multiple or no key was pressed!");
-    return GEM_KEY_NONE; // Multiple or no key has been pressed, exiting
-  }
+    writeBlockData(GP0, B11110000);
+    writeBlockData(GP1, B00111111);
 
-  // Stage 2: Scan on second 4 lanes, i.e. pull them high individually, to detect row
-  coordRow = -1; //Fallback value
-  for (int i = 0; i < 4; i++) {
-    writeBlockData(GP0, B00000001 << i); // Shift bits
-    if (readBlockData(GP0) & B11110000) { // Scanning terminates immediately after a pressed key is found. Only one bit in coordRow can be 1. Think about it!
-      coordRow = i;
-      break;
+    coordRow = (readBlockData(GP0) & B00001111);
+
+    writeBlockData(GP0, B00000000);
+    writeBlockData(GP1, B00000000);
+
+    if (coordRow != 1 && coordRow != 2 && coordRow != 4 && coordRow != 8) {
+        return -1;
     }
-  }
-  
-  writeBlockData(GP0, B00000000); // Cleanup
 
-  //Serial.print("Value of coordRow: ");
-  //Serial.println(coordRow);
+    coordRow = int(log(coordRow) / log(2)); // coordRow = 0, 1, 2, 3
 
-  if (coordRow == -1) { // If key is released too early
-    return GEM_KEY_NONE;
-  }
+    for (int i = 0; i < 4; i++) {
+        writeBlockData(GP0, B00010000 << i);
+        if (readBlockData(GP0) & B00001111) {
+            coordCol = i; // coordCol = 0, 1, 2, 3
+            break;
+        }
+    }
 
-  return keyBindings[coordRow][int(log(coordCol) / log(2)) - 4]; // maths, yeah...
+    writeBlockData(GP0, B00000000);
+
+    if (coordCol != -1) {
+        return keyBindings[coordRow][coordCol];
+    }
+
+    for (int i = 0; i < 10; i++) {
+        writeBlockData(GP1, B00000001 << i);
+        if (readBlockData(GP0) & B00001111) {
+            coordCol = i; // coordCol = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+            break;
+        }
+    }
+
+    writeBlockData(GP1, B00000000);
+
+    if (coordCol == -1) {
+        return -1; // Key was released too early
+    }
+
+    return keyBindings[coordRow][coordCol + 4];
+
 }
--- esp15c.ino
+++ esp15c.ino
@@ -7,8 +7,8 @@
 bool On = true;
 // var Display; (Object)
 bool DecimalSwap;
-double Stack = [0, 0, 0, 0];
-double StackI = [0, 0, 0, 0];
+double Stack = {0, 0, 0, 0};
+double StackI = {0, 0, 0, 0};
 double LastX = 0;
 double LastXI = 0;
 double Reg[60] = {0};
@@ -24,12 +24,42 @@ bool TemporaryDisplay = false;
 uint8_t Shift = 0; // 1=f 2=g
 Opcode (*Prefix)(uint8_t k); // var Prefix;
 Opcode (*OldPrefix)(uint8_t k); // var OldPrefix;
-// var LcdDisplay;
+// var LcdDisplay; (Object)
 uint8_t DisplayMode = 1; // 1=FIX 2=SCI 3=ENG
 uint8_t DisplayDigits = 4;
-int FullCircle = 360;
+unsigned short FullCircle = 360;
 double TrigFactor = M_PI / 180;
-bool Flags[] = [false, false, false, false, false, false, false, false, false, false];
+bool Flags[] = {false, false, false, false, false, false, false, false, false, false};
 bool User = false;
 bool Prgm = false;
- 
+Opcode Program[60]; // var Program = [null];
+unsigned short PC = 0;
+bool Running = false;
+// var RunTimer = null;
+bool BlinkOn = false;
+// var Blinker = null;
+unsigned short ReturnStack[60]; // var ReturnStack = [];
+uint8_t Result = 0;
+
+const double MAX = 9.999999999e99;
+const uint8_t MAX_MAG = 99;
+
+void setup() {
+    Serial.begin(115200);
+
+    randomSeed(analogRead(A0));
+
+    // the following will be moved into constructors in the future
+
+    u8g2.begin(); // Start U8g2 beforehand to display error messages to users
+
+    // I2C comm setup for further use of MCP23016 GPIO expander i.e. handleMCPKey()
+    Wire.begin(/*SDA=*/MCPSDA, /*SCL=*/MCPSCL);
+    Wire.setClock(200000); // !!Not sure about this value
+    writeBlockData(IODIR0, B00001111); // Configure first 4 pins as input and other 12 pins as output
+    writeBlockData(IODIR1, B00000000);
+}
+
+void loop() {
+
+}
